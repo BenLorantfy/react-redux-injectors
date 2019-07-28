6918008afaedd8f790eff1f3280606a7ad8fc469
Ben <benlorantfy@gmail.com>
2019-07-28 19:32:17 -0400

    Add typescript definition file and tweak api

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { forceReducerReload } from "./forceReducerReload";
 export { default as injectReducer, useInjectReducer } from "./injectReducer";
 export { default as injectSaga, useInjectSaga } from "./injectSaga";
 export { setupStoreForInjectors } from "./setupStoreForInjectors";
+export { SagaInjectionModes } from "./constants";
